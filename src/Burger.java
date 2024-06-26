0b850c951aef04bf4ba90163e4fffd160538c552
Your Name <sakshyambanjade@gmail.com>
2024-06-26 11:55:32 +0545

    completed-the-basic-bill-app

--- src/Burger.java
+++ src/Burger.java
@@ -1,6 +1,13 @@
 public class Burger {
     private int price;
     private Boolean veg;
+    private int cheese = 20;
+    private int topings = 30;
+    private int delivery = 20;
+    private int bprice;
+    private boolean ischeeseadded = false;
+    private boolean istopingsadded = false;
+    private boolean isdeliveryadded = false;
 
     public Burger(Boolean veg) {
         this.veg = veg;
@@ -9,9 +16,38 @@ public class Burger {
         } else {
             this.price = 200;
         }
+        bprice = this.price;
     }
 
-    public void getBurgerPrice() {
-        System.out.println(this.price);
+    public void addcheese() {
+        ischeeseadded = true;
+        this.price += cheese;
     }
+
+    public void addtopings() {
+        istopingsadded = true;
+        this.price += topings;
+    }
+
+    public void takeaway() {
+        isdeliveryadded = true;
+        this.price += delivery;
+    }
+
+    public void getbill() {
+        String bill = "";
+        System.out.println("Burger: " + bprice);
+        if (ischeeseadded) {
+            bill += "Extra cheeses added: " + cheese + "\n";
+        }
+        if (istopingsadded) {
+            bill += "Extra topings added: " + topings + "\n";
+        }
+        if (isdeliveryadded) {
+            bill += "Delivery charges: " + delivery + "\n";
+        }
+        bill += "Bill: " + this.price + "\n";
+        System.out.println(bill);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Burger type = new Burger(true);
-        type.getBurgerPrice();
+        Burger btype = new Burger(false); // Example for a vegetarian burger
+        btype.addcheese();
+        btype.addtopings();
+        btype.takeaway();
+        btype.getbill();
     }
 }
